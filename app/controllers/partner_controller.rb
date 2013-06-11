1166111d260d891ca40fa29b1c933862ae3ec2ee
Stephen Kunath <stephen.kunath@digitalreasoning.com>
2013-06-11 13:24:07 -0400

    fixes to display country in partner list

--- app/controllers/partner_controller.rb
+++ app/controllers/partner_controller.rb
@@ -1,6 +1,7 @@
 class PartnerController < ApplicationController
   def list_partners
     @partners = Partner.find(:all)
+    @partner_country_count = Partner.list_partner_countries()
   end
 
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -10,4 +10,12 @@ class Partner < ActiveRecord::Base
     end
     return countries.join(", ")
   end
+  
+  def self.list_partner_countries
+    return_data = self.find_by_sql("SELECT c.iso3 as name, count(*) as counter FROM partner_countries a
+          left outer join countries c on c.iso2 = a.iso_code
+          where c.iso3 != ''  group by c.iso3")
+    return return_data
+  end
+  
 end
